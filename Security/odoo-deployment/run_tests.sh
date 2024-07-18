105507d98f665a897c516435f7ea084bd5dce5eb
Genkiware <39473760+richcow2018@users.noreply.github.com>
2024-07-18 11:29:31 -0700

    Update run_tests.sh

--- Security/odoo-deployment/run_tests.sh
+++ Security/odoo-deployment/run_tests.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Running Odoo unit tests..."
-python3 /opt/bitnami/odoo/odoo-bin -c /opt/bitnami/odoo/conf/odoo.conf -d test_db --test-enable --stop-after-init --modules=small_custom_module
+python3 /odoo/apps/odoo/bin/odoo-bin -c /odoo/apps/odoo/conf/odoo-server.conf -d test_db --test-enable --stop-after-init --modules=small_custom_module
 
 echo "All tests passed!"
